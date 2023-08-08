de61ca86a5bd1f7734cb963b3540b7bf168b6af0
JeresonGCH <96240611+JeresonGCH@users.noreply.github.com>
2023-08-08 06:39:42 +0000

    Fixed syntax of index and main

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET", "POST"])  ##post is storing the data in python, get is trying to read data from python itself
+@app.route(
+  '/', methods=["GET", "POST"]
+)  ##post is storing the data in python, get is trying to read data from python itself
 def index():
   if request.method == "POST":  ##using post here because in html file, we are using post. Basically, python is checking if the file is post, it will store the messages. else, it will return the messages
     message = request.form['message']
@@ -12,9 +14,9 @@ def index():
       ##file close
     return render_template("index.html", message=message)
     ##the following lines are for when the method is "get"
-    with open('messages.txt', 'r') as file:
-      posts = file.readlines()
-    return render_template("index.html", posts=posts)
+  with open('messages.txt', 'r') as file:
+    posts = file.readlines()
+  return render_template("index.html", posts=posts)
 
 
 app.run(host='0.0.0.0', port=81)
